8ae3ed619a3cc3db1723bcafa4c630f6172a8807
x1ah <x1ahgxq@gmail.com>
2017-04-25 17:31:33 +0800

    新增celery配置

--- config.py
+++ config.py
@@ -4,4 +4,10 @@
 STORE_LOG = True
 
 # 存储日志文件绝对路径
-LOG_NAME = './log/QQRobot.log'
+LOG_NAME = '/tmp/QQRobot.log'
+
+# celery 配置
+class CeleryConfig(object):
+    BROKER_URL = 'redis://127.0.0.1:6379/5'
+    CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/6'
+
